2ba3795ee5e2630519ec0230c3e184bf8857396d
Ivan Uresti <talyfuantos@gmail.com>
2026-02-16 11:41:36 -0600

    añadir interceptor para agregar el token de autenticación en las solicitudes

--- react-peliculas/src/api/clienteAxios.ts
+++ react-peliculas/src/api/clienteAxios.ts
@@ -1,10 +1,23 @@
 import axios from 'axios';
+import { obtenerToken } from '../features/seguridad/utilidades/ManejadorJWT';
 
 const clienteAPI = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json'
     }
+});
+
+// Interceptor para agregar el token de autenticación a cada solicitud
+clienteAPI.interceptors.request.use((config) => {
+    const token = obtenerToken();
+
+    if (token) {
+        // Si el token existe, agregarlo al encabezado Authorization de la solicitud
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
 });
 
 export default clienteAPI;
